ba20787c1394704630caee0e6094da541aaede4d
oldmanye007 <xtpaye@gmail.com>
2025-02-15 17:11:03 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_dummy_lib',
+    name='dummy_lib',
     description= 'A dummy library for testing',
     version= '1.0.0',
     license='GNU General Public License v3.0',
