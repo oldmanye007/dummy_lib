72b60e461a3224c504721171e56fe071e41b01b2
oldmanye007 <xtpaye@gmail.com>
2025-02-15 16:44:18 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_dummy_lib',
+    description= 'A dummy library for testing',
+    version= '1.0.0',
+    license='GNU General Public License v3.0',
+    url='https://github.com/oldmanye007/dummy_lib',
+    author = 'oldmanye007',
+    packages=find_packages(),
+    install_requires=['numpy',
+                      'pandas',
+                      'scipy'],
+    python_requires='>=3.6, !=3.9.*'
+    )
