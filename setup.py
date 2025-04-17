b4e74de4647ae612329edff869ee5944afdf8846
oldmanye007 <xtpaye@gmail.com>
2025-04-16 23:17:54 -0500

    Update setup.py dependency

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='dummy_lib',
     description= 'A dummy library for testing',
-    version= '1.0.0',
+    version= '1.0.1',
     license='GNU General Public License v3.0',
     include_package_data=True,
     url='https://github.com/oldmanye007/dummy_lib',
     author = 'oldmanye007',
     packages=find_packages(),
-    install_requires=['numpy',
+    install_requires=['h5netcdf',
+                      'numpy',
                       'pandas',
                       'scipy'],
-    python_requires='>=3.6, !=3.9.*'
+    python_requires='>3.9.*'
     )
